988c7f45673c9f18eb7e2426fd621f19418e82c4
sole <sole@mozilla.com>
2016-04-14 09:16:22 -0300

    adjusting size to fit

--- examples/canvas-pixel-manipulation-and-record.js
+++ examples/canvas-pixel-manipulation-and-record.js
@@ -15,21 +15,37 @@ window.onload = function () {
   });
 };
 
+function toActual(value, dpi) {
+	return (Math.floor(value / 2)) + 'px';
+}
+
 function initCanvas(video) {
-  video.width = video.videoWidth / 2;
-  video.height = video.videoHeight / 2;
-  var width = video.videoWidth;
-  var height = video.videoHeight;
+	var outVideo = document.querySelector('video');
+  var dpi = window.devicePixelRatio || 1;
+  var videoWidth = video.videoWidth;
+  var videoHeight = video.videoHeight;
+  var availableWidth = window.innerWidth;
+  var half = availableWidth * 0.5;
+  var scale = half / videoWidth;
+
+  var finalWidth = Math.floor(half);
+  var finalHeight = Math.floor(videoHeight * scale);
 
   var canvas = document.querySelector('canvas');
-  canvas.width = width;
-  canvas.height = height;
+  canvas.width = finalWidth;
+  canvas.height = finalHeight;
+  var style = 'width: ' + toActual(finalWidth, dpi) + '; height: ' + toActual(finalHeight, dpi) + ';';
+  canvas.style = style;
+
+  video.width = finalWidth;
+  video.height = finalHeight;
+  outVideo.style = style;
 
   var ctx = canvas.getContext('2d');
   var draw = function () {
     requestAnimationFrame(draw);
-    ctx.drawImage(video, 0, 0, width, height);
-    applyFilter(ctx, width, height);
+	ctx.drawImage(video, 0, 0, video.videoWidth, video.videoHeight, 0, 0, finalWidth, finalHeight);
+    applyFilter(ctx, finalWidth, finalHeight);
   };
 
   draw();
@@ -38,8 +54,6 @@ function initCanvas(video) {
   recorder = new MediaRecorder(stream);
   recorder.addEventListener('dataavailable', function (evt) {
 	var outVideo = document.querySelector('video');
-	outVideo.width = canvas.width / 2;
-	outVideo.height = canvas.height / 2;
 	outVideo.src = URL.createObjectURL(evt.data);
 	outVideo.play();
   });
