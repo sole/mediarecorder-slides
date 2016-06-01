3f5dc97e966bc6822c3f484d79a7057dd35fdf6d
sole <sole@mozilla.com>
2016-06-01 14:38:52 +0100

    Add button to start demo to record-av demo

--- examples/record-av.js
+++ examples/record-av.js
@@ -1,26 +1,33 @@
 window.onload = function() {
-	navigator.mediaDevices.getUserMedia({
-		audio: true,
-		video: true
-	}).then(function (stream) {
-		var video = document.createElement('video');
-		video.src = URL.createObjectURL(stream);
-		document.body.appendChild(video);
-		video.play();
+	var button = document.querySelector('button');
+	button.onclick = demo;
 
-		var recorder = new MediaRecorder(stream);
-		recorder.addEventListener('dataavailable', e => {
-			video.src = URL.createObjectURL(e.data);
+	function demo() {
+		button.parentNode.removeChild(button);
+		navigator.mediaDevices.getUserMedia({
+			audio: true,
+			video: true
+		}).then(function (stream) {
+			var video = document.createElement('video');
+			video.src = URL.createObjectURL(stream);
+			document.body.appendChild(video);
 			video.play();
-			video.loop = true;
+
+			var recorder = new MediaRecorder(stream);
+			recorder.addEventListener('dataavailable', e => {
+				video.src = URL.createObjectURL(e.data);
+				video.play();
+				video.loop = true;
+			});
+			setTimeout(() => {
+				recorder.start();
+			}, 500);
+
+			setTimeout(() => {
+				recorder.stop();
+			}, 5000);
+
 		});
-		setTimeout(() => {
-			recorder.start();
-		}, 500);
+	}
 
-		setTimeout(() => {
-			recorder.stop();
-		}, 5000);
-
-	});
 };
