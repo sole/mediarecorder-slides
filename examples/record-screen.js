9d3dd0f367e3e135b28c5da8fefb5a60b5ce2f6f
sole <sole@mozilla.com>
2016-06-01 23:06:27 +0100

    ramp up record screen example

--- examples/record-screen.js
+++ examples/record-screen.js
@@ -4,26 +4,46 @@ window.onload = function() {
 			mediaSource: 'screen'
 		}
 	}).then(function (stream) {
-		var video = document.createElement('video');
-		video.src = URL.createObjectURL(stream);
-		document.body.appendChild(video);
-		video.play();
-
-		var recorder = new MediaRecorder(stream);
-		recorder.addEventListener('dataavailable', e => {
-			video.src = URL.createObjectURL(e.data);
-			video.play();
-			video.loop = true;
-		});
-		setTimeout(() => {
-			recorder.start();
-		}, 500);
-
-		setTimeout(() => {
-			recorder.stop();
-		}, 5000);
-
+		loopForever(recordClip, stream);
 	}).catch(function(e) {
 		console.log('ay', e);
 	});
+
+	function loopForever(fun, stream) {
+		fun(stream).then(() => {
+			setTimeout(() => {
+				loopForever(fun, stream);
+			}, 1000);
+		});
+	}
+
+	function recordClip(stream) {
+		return new Promise((ok, fail) => {
+
+			var recorder = new MediaRecorder(stream);
+
+			recorder.addEventListener('dataavailable', e => {
+				var v = document.createElement('video');
+				document.body.insertBefore(v, document.body.firstChild);
+
+				while(document.body.children.length > 3) {
+					document.body.removeChild(document.body.lastChild);
+				}
+
+				v.src = URL.createObjectURL(e.data);
+				v.play();
+				v.loop = true;
+				ok();
+			});
+			
+			recorder.start();
+
+			setTimeout(() => {
+				recorder.stop();
+			}, 1000);
+
+		});
+
+	}
+
 };
